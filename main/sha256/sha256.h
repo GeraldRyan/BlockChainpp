72485404a16c1980ff61680f2e43528b6d04bfd5
GeraldRyan <gerald.ryan40@yahoo.com>
2021-01-20 10:30:45 -0600

    (re) implementing SHA256 algorithm from zedwood.com for use in blockchain app

---
+++ main/sha256/sha256.h
@@ -0,0 +1,12 @@
+#ifndef SHA256_H
+#define SHA256_H
+#include <string>
+
+class SHA256 {
+   protected:
+    typedef unsigned char uint8;
+    typedef unsigned int uint32;
+    typedef unsigned long long uint64;
+
+
+};
