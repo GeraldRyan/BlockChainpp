6e85ddc9da8f2a02f24d43724c06d34cdb8f9443
GeraldRyan <gerald.ryan40@yahoo.com>
2021-01-23 23:37:57 -0600

    Added timer to main SHA256 test function. I want to toggle const for k values when finished

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,23 @@
+#include <chrono>
 #include <iostream>
-#include "sha256/sha256.h"
-#define el endl // try this
 
-using std::string;
+#include "sha256/sha256.h"
+#define el endl  // try this
+
 using std::cout;
 using std::endl;
+using std::string;
+using namespace std::chrono;
 
 // THIS WILL TEST SHA256 FUNCTION
-int main(int argc, char* argv[]){
-  string input = "grape";
-  string output1 = sha256(input);
+int main(int argc, char* argv[]) {
+    string input = "grape";
+    auto start = high_resolution_clock::now();
+    string output1 = sha256(input);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
 
-  cout << "sha256('"<< input << "'):" << output1 << endl;
-  return 0;
+    cout << "sha256('" << input << "'):" << output1 << endl;
+    cout << "Duration: " << duration.count() << endl;
+    return 0;
 }
